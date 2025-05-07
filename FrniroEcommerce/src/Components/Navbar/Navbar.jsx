f0041b2de49ce6c9d96a3510d1f36e9ceb304a87
Ibnushemsudin <musteshemsu@gmail.com>
2025-05-07 23:45:19 +0300

    added Navbar

---
+++ FrniroEcommerce/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <h1>Furniro</h1>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div>
+          <button>Search</button>
+          <button>Cart</button>
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
