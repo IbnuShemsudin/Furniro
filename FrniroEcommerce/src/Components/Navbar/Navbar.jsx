e4459a3e9ffc6bc6cc7c1647222d662a18ceb3e1
Ibnushemsudin <musteshemsu@gmail.com>
2025-05-09 01:38:16 +0300

    Some modification

--- FrniroEcommerce/src/Components/Navbar/Navbar.jsx
+++ FrniroEcommerce/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import { useState } from 'react' 
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 // import {search} from 'react-icons/fa'
 // import { cart } from 'react-icons/fa'
 
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className="navbar">
       <div className="logo">
@@ -26,11 +28,49 @@ function Navbar() {
           </li>
         </ul>
         <div>
-          <button>Search</button>
+          <button>Account</button>
           <button>Cart</button>
           <button>Login</button>
           <button>Sign Up</button>
         </div>
+      </div>
+      <div className="furnito_Menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="furnito_Menu_Container scale-up-center">
+            <div className="furnito_Menu_Container_2">
+              <ul className="furnito_Menu_Container_2_list">
+                <li>
+                  <a href="">Account</a>
+                </li>
+                <li>
+                  <a href="">Home</a>
+                </li>
+                <li>
+                  <a href="">Products</a>
+                </li>
+                <li>
+                  <a href="">About</a>
+                </li>
+                <li>
+                  <a href="">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
