e6404307d13cd5c39e74777b415f7e909dc520ca
Ibnushemsudin <musteshemsu@gmail.com>
2025-05-15 01:23:07 +0300

    Added furniture component

--- FrniroEcommerce/src/Components/Browse/Browse.jsx
+++ FrniroEcommerce/src/Components/Browse/Browse.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './browse.css'
-import Browse1 from '../../assets/browse1.jpg'
-import Browse2 from '../../assets/browse2.jpg'
-import Browse3 from '../../assets/browse3.jpg'
+import Browse1 from "../../assets/browse1.png"
+import Browse2 from "../../assets/browse2.png"
+import Browse3 from '../../assets/browse3.png'
 function Browse() {
   return (
-    <div>
-        <div className="browse">
-        <h1>Browse The Range</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div className="browse-container">
+      <h1>Browse The Range</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div className="browse">
         <div className="browse-categories">
-          <img src={Browse1} alt="" />
+          <img src={Browse2} alt="" />
           <h2>Dinning</h2>
         </div>
         <div className="browse-categories">
@@ -18,9 +18,13 @@ function Browse() {
           <h2>Dinning</h2>
         </div>
         <div className="browse-categories">
-          <img src={Browse3} alt="" />
+          <img src={Browse2} alt="" />
           <h2>Dinning</h2>
         </div>
+        {/* <div className="browse-categories">
+          <img src={Browse3} alt="" />
+          <h2>Dinning</h2>
+        </div> */}
       </div>
     </div>
   );
