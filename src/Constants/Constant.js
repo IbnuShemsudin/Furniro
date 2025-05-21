90b9367d074f32f6205ebe4a6146e48ca7534ed9
Ibnushemsudin <musteshemsu@gmail.com>
2025-05-21 23:22:57 +0300

    Created Constant.js

---
+++ src/Constants/Constant.js
@@ -0,0 +1,87 @@
+import product1 from "../assets/images/product1.png";
+// import product2 from "../assets/images/product2.png"; // Added imports
+// import product3 from "../assets/images/product3.png";
+// import product4 from "../assets/images/product4.png";
+// import product5 from "../assets/images/product5.png";
+// import product6 from "../assets/images/product6.png";
+// import product7 from "../assets/images/product7.png";
+// import product8 from "../assets/images/product8.png";
+// import product9 from "../assets/images/product9.png";
+
+const products = [
+  {
+    image: product1,
+    title: "Syltherine",
+    description: "Stylish cafe chair",
+    price: 2500000, // Stored as a number
+  },
+  {
+    image: product1,
+    title: "Another Chair",
+    description: "A different chair",
+    price: 1500000,
+  },
+  {
+    image: product1,
+    title: "Nice Table",
+    description: "A very nice table",
+    price: 3000000,
+  },
+  {
+    image: product1,
+    title: "Comfy Sofa",
+    description: "Comfortable sofa",
+    price: 5000000,
+  },
+  {
+    image: product1,
+    title: "Modern Chair",
+    description: "Modern style chair",
+    price: 2800000,
+  },
+  {
+    image: product1,
+    title: "Classic Table",
+    description: "Classic design table",
+    price: 3200000,
+  },
+  {
+    image: product1,
+    title: "Elegant Sofa",
+    description: "Elegant and stylish sofa",
+    price: 6000000,
+  },
+  {
+    image: product1,
+    title: "Rustic Chair",
+    description: "Rustic wooden chair",
+    price: 1800000,
+  },
+  {
+    image: product1,
+    title: "Luxury Sofa",
+    description: "Luxurious sofa design",
+    price: 7500000,
+  },
+];
+
+
+// const categories = [
+//   {
+//     name: "Living Room",
+//     image: "https://example.com/living-room.jpg",
+//   },
+//   {
+//     name: "Bedroom",
+//     image: "https://example.com/bedroom.jpg",
+//   },
+//   {
+//     name: "Dining Room",
+//     image: "https://example.com/dining-room.jpg",
+//   },
+//   {
+//     name: "Office",
+//     image: "https://example.com/office.jpg",
+//   },
+// ];
+export { products };
